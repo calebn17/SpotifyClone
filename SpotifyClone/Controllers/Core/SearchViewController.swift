1f064da3151828007656b00eb3a3c1cd79e2c842
Caleb Ngai <caleb.ngai@flosports.tv>
2022-04-15 17:21:57 -0500

    start on search

--- SpotifyClone/Controllers/Core/SearchViewController.swift
+++ SpotifyClone/Controllers/Core/SearchViewController.swift
@@ -7,14 +7,74 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+    
+    
+    
+    let searchController: UISearchController = {
+        let vc = UISearchController(searchResultsController: SearchResultsViewController())
+        vc.searchBar.placeholder = "Songs, Artists, Albums"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        return vc
+    }()
+    
+    private let collectionView: UICollectionView = UICollectionView(frame: .zero,
+                                                                    collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: {_, _ -> NSCollectionLayoutSection? in
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(180)), subitem: item, count: 2)
+        return NSCollectionLayoutSection(group: group)
+    }))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .systemBackground
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    //Actively update each character that is typed
+    func updateSearchResults(for searchController: UISearchController) {
+        //Making sure there is text, and that the input isn't just empty white spaces
+        guard let resultsController = searchController.searchResultsController as? SearchResultsViewController,
+            let query = searchController.searchBar.text,
+                !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        //resultsController.update(with: results)
+        print(query)
+        //Perform Search
+       // APICaller.shared.search
+    }
 
 
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
 }
