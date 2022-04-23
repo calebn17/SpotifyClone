74de19cfe7a2512fb364ca8f7abb1d6c4fa53a6d
Caleb Ngai <caleb.ngai@flosports.tv>
2022-04-23 14:23:29 -0500

    add more code around Library Playlists

--- SpotifyClone/Controllers/Other/Library/LibraryPlaylistViewController.swift
+++ SpotifyClone/Controllers/Other/Library/LibraryPlaylistViewController.swift
@@ -11,10 +11,23 @@ class LibraryPlaylistViewController: UIViewController {
 
     var playlists: [Playlist] = []
     
+    private let noPlaylistsView = ActionLabelView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
+        setUpNoPlaylistsView()
+        fetchCurrentPlaylists()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        noPlaylistsView.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
+        noPlaylistsView.center = view.center
+       
+    }
+    
+    private func fetchCurrentPlaylists() {
         APICaller.shared.getCurrentUserPlaylists { [weak self] result in
             DispatchQueue.main.async {
                 switch result {
@@ -25,20 +38,49 @@ class LibraryPlaylistViewController: UIViewController {
                     print(error.localizedDescription)
                 }
             }
-           
         }
+    }
+    
+    private func setUpNoPlaylistsView() {
+        noPlaylistsView.configure(with: ActionLabelViewViewModel(text: "You don't have any playlists yet", actionTitle: "Create"))
+        view.addSubview(noPlaylistsView)
+        noPlaylistsView.delegate = self
     }
     
     private func updateUI() {
         if playlists.isEmpty {
             //Show label
+            noPlaylistsView.isHidden = false
         }
         else {
             //Show table
         }
     }
-    
+}
 
-  
-
+extension LibraryPlaylistViewController: ActionLabelViewDelegate {
+    func actionLabelViewDidTapButton(_ actionView: ActionLabelView) {
+        //Show Creation UI
+        let alert = UIAlertController(title: "New Playlists", message: "Enter playlist name", preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "Playlist..."
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Create", style: .default, handler: { _ in
+            guard let field = alert.textFields?.first,
+                  let text = field.text,
+                  !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+                      return
+                  }
+            APICaller.shared.createPlaylists(with: text) { success in
+                if success {
+                    //Refresh list of playlists
+                }
+                else {
+                    print("Failed to create playlist")
+                }
+            }
+        }))
+    }
 }
--- SpotifyClone/Managers/APICaller.swift
+++ SpotifyClone/Managers/APICaller.swift
@@ -94,7 +94,36 @@ final class APICaller {
         }
     }
     public func createPlaylists(with name: String, completion: @escaping (Bool) -> Void) {
-        
+        getCurrentUserProfile { [weak self] result in
+            switch result {
+            case .success(let profile):
+                let urlString = Constants.baseAPIURL + "/users/\(profile.id)/playlists"
+                
+                self?.createRequest(with: URL(string: urlString), type: .POST){ baseRequest in
+                    
+                    var request = baseRequest
+                    let json = ["name": name]
+                    request.httpBody = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed)
+                    
+                    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                        guard let data = data, error == nil else {completion(false); return}
+                        
+                        do {
+                            let result = try JSONDecoder().decode(LibraryPlaylistsResponse.self, from: data)
+                            completion(true)
+                        }
+                        catch {
+                            //passing the error to the completion handler
+                            print(error.localizedDescription)
+                            completion(false)
+                        }
+                    }
+                    task.resume()
+                }
+            case.failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     public func addTrackToPlaylists(track: AudioTrack, playlist: Playlist, completion: @escaping (Bool) -> Void) {
         
